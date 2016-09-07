cdd97a8c42b580eda7ff76db4b6144c71f3fe89a
Je-Hoon Song <song.jehoon@gmail.com>
2016-09-07 17:02:57 +0900

    seperate make_range and combination

--- dataset/demo/data.py
+++ dataset/demo/data.py
@@ -14,8 +14,16 @@ from os.path import join,dirname
 treatment_file = join(dirname(__file__), 'Demo_drug_response.csv')
 output_dir = dirname(__file__)
 
+import pytest 
+import glob
+import json 
 
-def make_inputdata():
+from pdb import set_trace
+
+from sbie_weinberg.module import dream2015
+
+
+def make_range_demo(outfile='range.json'):
 
     df = pd.read_csv(treatment_file)
 
@@ -25,13 +33,47 @@ def make_inputdata():
     drugs = set(df['Compound'])
     drugs = [el for el in drugs]
 
-    input_range = { 'cells': celltypes, 'drugs': drugs } 
+    input_range = {'demo': { 'cell': celltypes, 'drug': drugs }}
 
-    with open('input_range.json', 'w') as outfile: 
-        json.dump(input_range, outfile, indent=4, sort_keys=True, \
+    with open(outfile, 'w') as outfobj: 
+        json.dump(input_range, outfobj, indent=4, sort_keys=True, \
             separators=(',',':'))
 
-    combi_numbers = [1, 2]
+                        
+def make_range_dream2015(outfile='range_dream2015.json'): 
+
+    datadir = join(dirname(dream2015.__file__), 
+        'code-with-inputdata')
+
+    """ this module calculates the range of cells and drugs from dataset """
+    df1 = pd.read_csv(datadir+'/THERAPY_TRAINSET.CSV') 
+    df2 = pd.read_csv(datadir+'/THERAPY_TESTSET.CSV')
+
+    cells = set( df1['CELL_LINE'].tolist() + df2['CELL_LINE'].tolist() )
+    drugs = set( df1['COMPOUND_A'].tolist() + df2['COMPOUND_B'].tolist() )
+
+    simul_range = { 
+        'dream2015': {
+            'cell': [el for el in cells], 
+            'drug': [el for el in drugs], 
+            }
+        }
+
+    with open('range_dream2015.json', 'w') as foutjson:
+        json.dump(simul_range, foutjson, indent=4, sort_keys=True, separators=(',', ':'))    
+
+
+def make_inputdata_combination(infile='range_dream2015.json', combi_numbers=[2], \
+        outputheader='demoinput_dream2015_'):
+
+    # combi_numbers = [ 2 ]
+    # outputheader = 'demoinput_dream2015_'
+    
+    with open(infile, 'r') as finp: 
+        datadict = json.load(finp)
+
+    drugs = datadict['dream2015']['drug']
+    cells = datadict['dream2015']['cell']
 
     drugs_list = [] 
 
@@ -40,7 +82,7 @@ def make_inputdata():
             drugs_list.append(d)
 
     inputjson_list = [] 
-    for thiscell in celltypes: 
+    for thiscell in cells: 
         for drugmembers in drugs_list: 
             inputjson = {
                 'input': {
@@ -51,9 +93,10 @@ def make_inputdata():
             inputjson_list.append(inputjson)
 
     for i,inputjson in enumerate(inputjson_list):
-        outputfilename = join(output_dir, 'demoinput_%d.json' % i)        
+        outputfilename = join(output_dir, outputheader+'%d.json' % i)        
         with open(outputfilename, 'w') as outfile: 
             json.dump(inputjson, outfile, indent=4, sort_keys=True,
                 separators=(',',':'))
+    pass
 
-            
+
