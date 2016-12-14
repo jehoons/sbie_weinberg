7a0b71a6077e0a0edc8a0595c0366f04cc598143
ChunKyung Lee <ChunKyung Lee>
2016-12-14 13:36:38 +0900

    Add phpinfo.php

---
+++ webapp/htdocs/modules/install/phpinfo.php
@@ -0,0 +1,16 @@
+<?php
+/* --------------------------------
+
+module/install/install.php
+MySQL Database table 설치 페이지.
+
+-------------------------------- */
+
+//__CL__ 정의 되지 않았다면 false 를 return.
+if(!defined('__CL__')) exit();
+?>
+
+<div id="cl-content">
+	<div id="cl-content-title">PHP Info</div>
+<?php echo phpinfo();?>
+</div>
